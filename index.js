85e8231099539f15d2d9129b857e7c22db02eff1
Ayush Dev <ayushdev.in@gmail.com>
2024-08-26 20:17:20 +0530

    resolved rebase conflicts

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+// const { token } = require("./config.json");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const token = process.env.TOKEN;
 const { waitForDebugger } = require("node:inspector");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
